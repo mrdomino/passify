be9145ad3fc4972885068195c48ec564a651084d
Steven Dee <steve@smartercode.net>
2014-07-16 17:45:37 -0700

    Layout skeleton

--- src/main/scala/MainActivity.scala
+++ src/main/scala/MainActivity.scala
@@ -1,13 +1,28 @@
 package org.wholezero.passable
 
-import _root_.android.app.Activity
-import _root_.android.os.Bundle
+import android.app.Activity
+import android.os.Bundle
+import android.text.Editable
+import org.scaloid.common._
+import scala.language.postfixOps
 
-class MainActivity extends Activity with TypedActivity {
+class MainActivity extends SActivity with TypedActivity {
+  def passify(masterPass : Editable, username : Editable, site : Editable) {
+    toast(masterPass + " " + username + " " + site)
+  }
   override def onCreate(bundle: Bundle) {
     super.onCreate(bundle)
-    setContentView(R.layout.main)
+    setContentView(new SVerticalLayout {
+      STextView(R.string.header_doc).textSize(24.5 sp).<<.marginBottom(25 dip).>>
+      STextView(R.string.master_passphrase)
+      val masterPass = SEditText() inputType TEXT_PASSWORD
+      STextView(R.string.username)
+      val username = SEditText() singleLine true
+      STextView(R.string.site)
+      val site = SEditText() singleLine true
+      SButton(R.string.passify, passify(masterPass.text, username.text, site.text))
 
-    findView(TR.textview).setText("hello, world!")
+      masterPass requestFocus ()
+    })
   }
 }
