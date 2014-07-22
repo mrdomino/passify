4e7ae4bb1b098f21008fb35a80a7db76c20a0c25
Steven Dee <steve@smartercode.net>
2014-07-22 13:03:32 -0700

    jni interface for keccak

---
+++ src/main/scala/Keccak1600.scala
@@ -0,0 +1,27 @@
+package org.wholezero.passify
+
+trait Sponge {
+  def absorb(bs : Seq[Byte]) : Unit
+  def squeeze(bits : Integer) : Seq[Byte]
+}
+
+object Keccak1600 {
+  System.loadLibrary("Keccak1600")
+
+  private class KeccakSponge(val r : Integer) extends Sponge {
+    private var sptr : Integer = 0
+
+    @native private def init : Unit = {}
+    @native override def absorb(bs : Seq[Byte]) : Unit
+    @native override def squeeze(bits : Integer) : Seq[Byte]
+
+    init
+  }
+
+  def apply(r : Integer) : Sponge = {
+    require(r <= 1600)
+    require((r % 8) == 0)
+    require(r > 0)
+    new KeccakSponge(r)
+  }
+}
