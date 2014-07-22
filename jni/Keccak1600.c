bf21ae081d1cf4b2f4f632949d37bc2ed7a12f4c
Steven Dee <steve@smartercode.net>
2014-07-22 13:31:11 -0700

    Skeleton implementation

---
+++ jni/Keccak1600.c
@@ -0,0 +1,47 @@
+#include "org_wholezero_passify_Keccak1600_KeccakSponge.h"
+
+/*
+ * Class:     org_wholezero_passify_Keccak1600_KeccakSponge
+ * Method:    init
+ * Signature: ()V
+ */
+JNIEXPORT void JNICALL
+Java_org_wholezero_passify_Keccak1600_00024KeccakSponge_init
+(JNIEnv *e, jobject s)
+{
+}
+
+/*
+ * Class:     org_wholezero_passify_Keccak1600_KeccakSponge
+ * Method:    absorb
+ * Signature: (Lscala/collection/Seq;)V
+ */
+JNIEXPORT void JNICALL
+Java_org_wholezero_passify_Keccak1600_00024KeccakSponge_absorb
+(JNIEnv *e, jobject s, jobject bs)
+{
+}
+
+/*
+ * Class:     org_wholezero_passify_Keccak1600_KeccakSponge
+ * Method:    absorbLastFewBits
+ * Signature: (B)V
+ */
+JNIEXPORT void JNICALL
+Java_org_wholezero_passify_Keccak1600_00024KeccakSponge_absorbLastFewBits
+(JNIEnv *e, jobject s, jbyte b)
+{
+}
+
+
+/*
+ * Class:     org_wholezero_passify_Keccak1600_KeccakSponge
+ * Method:    squeeze
+ * Signature: (I)Lscala/collection/Seq;
+ */
+JNIEXPORT jobject JNICALL
+Java_org_wholezero_passify_Keccak1600_00024KeccakSponge_squeeze
+(JNIEnv *e, jobject s, jint n)
+{
+  return 0;
+}
