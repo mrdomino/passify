64ca0a31e16ca622a23430f69fb123125bc7ecda
Steven Dee <steve@smartercode.net>
2014-07-22 13:57:57 -0700

    Better API for Keccak1600

--- src/main/scala/Keccak1600.scala
+++ src/main/scala/Keccak1600.scala
@@ -1,6 +1,6 @@
 package org.wholezero.passify
 
-trait Sponge {
+trait Keccak1600 {
   def absorb(bs : Seq[Byte]) : Unit
   def absorbLastFewBits(b : Byte) : Unit
   def squeeze(bytes : Int) : Seq[Byte]
@@ -9,10 +9,10 @@ trait Sponge {
 object Keccak1600 {
   System.loadLibrary("Keccak1600")
 
-  private class KeccakSponge(val r : Integer) extends Sponge {
+  private class KeccakSponge(val r : Integer) extends Keccak1600 {
     private var sptr : Int = 0
 
-    @native private def init : Unit = {}
+    @native protected def init : Unit
     @native override def absorb(bs : Seq[Byte]) : Unit
     @native override def absorbLastFewBits(b : Byte) : Unit
     @native override def squeeze(bytes : Int) : Seq[Byte]
@@ -20,7 +20,7 @@ object Keccak1600 {
     init
   }
 
-  def apply(r : Integer) : Sponge = {
+  def apply(r : Integer) : Keccak1600 = {
     require(r <= 1600)
     require((r % 8) == 0)
     require(r > 0)
